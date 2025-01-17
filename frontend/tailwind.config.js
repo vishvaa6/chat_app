d3b46c11c66128d06f43126a08e71d2215c8ba6f
vishvaa6 <vishvaav006@gmail.com>
2025-01-17 17:13:00 +0530

    1.0.0

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
@@ -8,6 +9,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
